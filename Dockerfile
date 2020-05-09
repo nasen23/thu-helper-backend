c1e06167571e3c3f815a43b4136c01698b075ed3
Sen Na <i@nasen.xyz>
2020-05-09 10:49:43 +0800

    Allow program to run both natively or inside docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:14
+ENV DATABASE_HOST pgdb
 WORKDIR /code
 COPY . .
 RUN yarn install --registry https://registry.npm.taobao.org
--- index.ts
+++ index.ts
@@ -1,50 +1,57 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import router from './router'
-import { createConnection } from 'typeorm'
+import { getConnectionOptions, createConnection } from 'typeorm'
 import * as morgan from 'morgan'
 import * as session from 'express-session'
 import 'reflect-metadata'
 
-createConnection()
-  .then(_ => {
-    const app: express.Application = express()
+;(async () => {
+  const connectionOptions = await getConnectionOptions()
+  if (process.env.DATABASE_HOST) {
+    Object.assign(connectionOptions, { host: process.env.DATABASE_HOST })
+  }
 
-    app.use(morgan('dev'))
-    app.use(bodyParser.json())
-    app.use(
-      (
-        err: Error,
-        _req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        if (err instanceof SyntaxError) {
-          res.sendStatus(400)
-        } else {
-          next()
+  createConnection(connectionOptions)
+    .then(_ => {
+      const app: express.Application = express()
+
+      app.use(morgan('dev'))
+      app.use(bodyParser.json())
+      app.use(
+        (
+          err: Error,
+          _req: express.Request,
+          res: express.Response,
+          next: express.NextFunction
+        ) => {
+          if (err instanceof SyntaxError) {
+            res.sendStatus(400)
+          } else {
+            next()
+          }
         }
-      }
-    )
+      )
 
-    app.use(
-      session({
-        secret: 'thu_helper',
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false,
+      app.use(
+        session({
+          secret: 'thu_helper',
+          cookie: { maxAge: 60000 },
+          resave: false,
+          saveUninitialized: false,
+        })
+      )
+
+      app.use(router)
+
+      // catching 404
+      app.use((_, res) => {
+        res.status(404).send('Not Found')
       })
-    )
 
-    app.use(router)
-
-    // catching 404
-    app.use((_, res) => {
-      res.status(404).send('Not Found')
+      app.listen(process.env.PORT || 3000, () => {
+        console.log('App running on port 3000')
+      })
     })
-
-    app.listen(process.env.PORT || 3000, () => {
-      console.log('App running on port 3000')
-    })
-  })
-  .catch(err => console.log(err))
+    .catch(err => console.log(err))
+})()
